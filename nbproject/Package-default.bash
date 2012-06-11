1c8c38051558ab134dd71e0cba371125e36ec14d
Andrew Pullin <pullin@berkeley.edu>
2012-06-11 15:06:44 -0700

    Fixing MPLAB project

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/basestation.X.${IMAGE_TYPE}.elf
-OUTPUT_BASENAME=basestation.X.${IMAGE_TYPE}.elf
-PACKAGE_TOP_DIR=basestation.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/basestation.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=basestation.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=basestation/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/basestation.x/bin
+makeDirectory ${TMPDIR}/basestation/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/basestation.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/basestation.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/basestation.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/basestation.tar *
 checkReturnCode
 
 # Cleanup
